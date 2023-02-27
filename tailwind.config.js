6c01eefc3f8781bdb6a047c551a99f7e4135b3e5
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-02-27 10:19:56 +0200

    Added images & custom styles

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 module.exports = {
   content: ["./src/*"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        customGrey: "hsl(0, 0%, 63%)",
+        customBlack: "hsl(0, 0%, 0%)",
+        customWhite: "hsl(0, 0%, 100%)",
+        customDarkgrey: "hsl(0, 0%, 27%)"
+      }
+    },
   },
   plugins: [],
 }
