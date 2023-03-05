075dc753f8e05a2122de9300ceb010f964f998a0
Zeyad <ZeyadMohamed.BUS.ACC@Gmail.com>
2023-03-06 01:14:55 +0200

    Moved the Image Buttons from the Shop Component to the Home Component

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,6 +1,11 @@
 const Home = () => {
     return (
-        <section className="col-span-2 row-span-1 bg-cover bg-center bg-no-repeat" style={{backgroundImage: `url(${require("../../Images/desktop-image-hero-1.jpg")})`}}></section>
+        <section className="col-span-2 row-span-1 bg-cover bg-center bg-no-repeat relative" style={{backgroundImage: `url(${require("../../Images/desktop-image-hero-1.jpg")})`}}>
+            <div className="absolute bottom-0 -right-1/4 flex w-1/4">
+                <button className="w-1/2 h-20 bg-customBlack cursor-pointer grid place-items-center duration-500 hover:bg-customDarkGrey"><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M13 0L1 12l12 12" stroke="hsl(0, 0%, 100%)" fill="none" fillRule="evenodd"/></svg></button>
+                <button className="w-1/2 h-20 bg-customBlack cursor-pointer grid place-items-center duration-500 hover:bg-customDarkGrey"><svg width="14" height="24" xmlns="http://www.w3.org/2000/svg"><path d="M1 0l12 12L1 24" stroke="hsl(0, 0%, 100%)" fill="none" fillRule="evenodd"/></svg></button>
+            </div>
+        </section>
     )
 }
 
